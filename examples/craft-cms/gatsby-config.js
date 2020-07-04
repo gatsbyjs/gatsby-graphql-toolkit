6a6fcdacccabbb7aca3cc634580d007f386b2d30
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2020-07-04 16:14:13 +0700

    Remove unused files from Craft CMS example

--- examples/craft-cms/gatsby-config.js
+++ examples/craft-cms/gatsby-config.js
@@ -1,12 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-  ],
+  plugins: [],
 }
